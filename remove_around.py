1490e1b7337e3d47c0fce69424c63238516c7fc3
AlexeyZag <72604307+AlexeyZag@users.noreply.github.com>
2020-10-19 16:19:10 +0300

    Update remove_around.py

--- remove_around.py
+++ remove_around.py
@@ -4,36 +4,80 @@ class RemoveSq1:   # удаляет числа из множества дост�
         self.seq = seq
     @property
     def remover1(self):
-        try:
-            self.seq.remove(self.place1 + 1)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 + 9)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 + 10)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 + 11)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 - 1)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 - 9)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 - 10)
-        except KeyError:
-            pass
-        try:
-            self.seq.remove(self.place1 - 11)
-        except KeyError:
-            pass
+        if self.place1 % 10 != 0 and self.place1 % 10 != 9:
+            try:
+                self.seq.remove(self.place1 + 1)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 9)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 10)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 11)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 1)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 9)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 10)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 11)
+            except KeyError:
+                pass
+        elif self.place1 % 10 == 0:
+            try:
+                self.seq.remove(self.place1 + 1)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 10)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 11)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 9)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 10)
+            except KeyError:
+                pass
+        elif self.place1 % 10 == 9:
+            try:
+                self.seq.remove(self.place1 - 1)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 10)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 - 11)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 9)
+            except KeyError:
+                pass
+            try:
+                self.seq.remove(self.place1 + 10)
+            except KeyError:
+                pass
+
         return self.seq
