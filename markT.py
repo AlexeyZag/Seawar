8927bc5a21d4384cfb3d698d56560eeec5d5751e
AlexeyZag <72604307+AlexeyZag@users.noreply.github.com>
2020-11-06 18:38:16 +0300

    Update markT.py

--- markT.py
+++ markT.py
@@ -6,99 +6,35 @@ class MarkT:  # маркерует поля вокруг подбитого ко
     def marker(self):
         for i in self.subseq:
             if i % 10 != 0 and i % 10 != 9:
-                try:
-                    if self.seqVis[i + 1] != 'X':
-                        self.seqVis[i + 1] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i + 9] != 'X':
-                        self.seqVis[i + 9] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i + 10] != 'X':
-                        self.seqVis[i + 10] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i + 11] != 'X':
-                        self.seqVis[i + 11] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i - 1] != 'X':
-                        self.seqVis[i - 1] = 'T'
-                except IndexError:
-                    pass
+                self.exception(i, 1)
+                self.exception(i, 9)
+                self.exception(i, 10)
+                self.exception(i, 11)
+                self.exception(i, -1)
                 if i // 10 != 0:
-                    try:
-                        if self.seqVis[i - 9] != 'X':
-                            self.seqVis[i - 9] = 'T'
-                    except IndexError:
-                        pass
-                    try:
-                        if self.seqVis[i - 10] != 'X':
-                            self.seqVis[i - 10] = 'T'
-                    except IndexError:
-                        pass
-                    try:
-                        if self.seqVis[i - 11] != 'X':
-                            self.seqVis[i - 11] = 'T'
-                    except IndexError:
-                        pass
+                    self.exception(i, -9)
+                    self.exception(i, -10)
+                    self.exception(i, -11)
             elif i % 10 == 0:
-                try:
-                    if self.seqVis[i + 1] != 'X':
-                        self.seqVis[i + 1] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i + 10] != 'X':
-                        self.seqVis[i + 10] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i + 11] != 'X':
-                        self.seqVis[i + 11] = 'T'
-                except IndexError:
-                    pass
+                self.exception(i, 1)
+                self.exception(i, 10)
+                self.exception(i, 11)
                 if i // 10 != 0:
-                    try:
-                        if self.seqVis[i - 9] != 'X':
-                            self.seqVis[i - 9] = 'T'
-                    except IndexError:
-                        pass
-                    try:
-                        if self.seqVis[i - 10] != 'X':
-                            self.seqVis[i - 10] = 'T'
-                    except IndexError:
-                        pass
+                    self.exception(i, -9)
+                    self.exception(i, -10)
             elif i % 10 == 9:
-                try:
-                    if self.seqVis[i - 1] != 'X':
-                        self.seqVis[i - 1] = 'T'
-                except IndexError:
-                    pass
+                self.exception(i, -1)
                 if i // 10 != 0:
-                    try:
-                        if self.seqVis[i - 10] != 'X':
-                            self.seqVis[i - 10] = 'T'
-                    except IndexError:
-                        pass
-                    try:
-                        if self.seqVis[i - 11] != 'X':
-                            self.seqVis[i - 11] = 'T'
-                    except IndexError:
-                        pass
-                try:
-                    if self.seqVis[i + 9] != 'X':
-                        self.seqVis[i + 9] = 'T'
-                except IndexError:
-                    pass
-                try:
-                    if self.seqVis[i + 10] != 'X':
-                        self.seqVis[i + 10] = 'T'
-                except IndexError:
-                    pass
+                    self.exception(i, -10)
+                    self.exception(i, -11)
+                self.exception(i, 9)
+                self.exception(i, 10)
+        return self.seqVis
+
+    def exception(self, i, n):
+        try:
+            if self.seqVis[i + n] != 'X':
+                self.seqVis[i + n] = 'T'
+        except IndexError:
+            pass
         return self.seqVis
